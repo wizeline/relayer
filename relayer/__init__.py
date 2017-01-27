63b1f4f1158380f7ac515ac89210d68c739163ad
Fernando Padilla <rpfernando@wizeline.com>
2017-01-27 11:15:20 -0600

    Bump version

--- relayer/__init__.py
+++ relayer/__init__.py
@@ -3,7 +3,7 @@ from kafka import KafkaProducer
 from .event_emitter import EventEmitter
 from .exceptions import ConfigurationError
 
-__version__ = '0.1.2'
+__version__ = '0.1.3'
 
 
 class Relayer(object):
