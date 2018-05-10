3650cca3c8b4206b32f98812f5278acd0e42d4a5
Cesar Landeros Delgado <cesar.landeros@wizeline.com>
2018-05-10 12:37:39 -0500

    bump version to 2.0.3

--- relayer/__init__.py
+++ relayer/__init__.py
@@ -4,7 +4,7 @@ from .event_emitter import EventEmitter
 from .exceptions import ConfigurationError
 from .logging import logger as log
 
-__version__ = '2.0.2'
+__version__ = '2.0.3'
 
 
 class Relayer(object):
