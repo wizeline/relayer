97aefdfc29de51d8acaf6f9aab7c672b646a3930
ivansanchezg <ivan.sanchez@wizeline.com>
2018-06-05 14:19:21 -0500

    Update tests with assertions and type hints

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -7,11 +7,11 @@ from . import BaseTestCase
 
 class TestUtils(BaseTestCase):
 
-    def test_elapsed_time_in_milliseconds(self):
+    def test_elapsed_time_in_milliseconds(self) -> None:
         start_time = datetime(2016, 6, 21, 15, 12, 5, 500000)
         end_time = datetime(2016, 6, 21, 15, 12, 6, 600000)
         milliseconds = utils.get_elapsed_time_in_milliseconds(start_time, end_time)
-        milliseconds.should.equal(1100.0)
+        assert milliseconds == 1100.0
         end_time = datetime(2016, 6, 21, 15, 12, 6, 600500)
         milliseconds = utils.get_elapsed_time_in_milliseconds(start_time, end_time)
-        milliseconds.should.equal(1100.5)
+        assert milliseconds == 1100.5
