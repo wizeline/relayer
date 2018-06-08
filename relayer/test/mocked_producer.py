6e0706cec50b122b69b5350ec79f8310d62b25d9
ivansanchezg <ivan.sanchez@wizeline.com>
2018-06-08 12:57:00 -0500

    Fix annotation

--- relayer/test/mocked_producer.py
+++ relayer/test/mocked_producer.py
@@ -1,11 +1,10 @@
 from typing import Dict, Any, List, Tuple
-
 from collections import defaultdict
 
 
 class MockedProducer(object):
     def __init__(self) -> None:
-        self.produced_messages = defaultdict(list)  # type: Dict[Any, List[Tuple[Any, ...]]]
+        self.produced_messages: Dict[Any, List[Tuple[Any, ...]]] = defaultdict(list)
         self.flushed = False
 
     def send(self, topic: str, value: str = None, key: str = None) -> None:
