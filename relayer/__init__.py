4849c7ccaadc2266b0241a862d473f5837fc8d9c
Ixai Lanzagorta Ochoa <ixai.lanzagorta@gmail.com>
2018-06-19 09:51:26 -0700

    Version 2.0.4
    Includes:
    - Improve dependency management for development and pypi releases (#38,
        #43)
    - Improve code health (format + type hints) (#41, #42)

--- relayer/__init__.py
+++ relayer/__init__.py
@@ -5,7 +5,7 @@ from .event_emitter import EventEmitter
 from .exceptions import ConfigurationError
 from .logging import logger as log
 
-__version__ = '2.0.3'
+__version__ = '2.0.4'
 
 
 class Relayer(object):
